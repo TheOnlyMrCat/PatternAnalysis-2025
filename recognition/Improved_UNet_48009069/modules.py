252af08c949f9f3c1f93ae9580d72d4de48656b5
Max Guppy <m.guppy@student.uq.edu.au>
2025-10-23 10:36:43 +1000

    Remove obsolete SimpleUNet module

--- recognition/Improved_UNet_48009069/modules.py
+++ recognition/Improved_UNet_48009069/modules.py
@@ -1,60 +1,6 @@
 import torch
 import torch.nn as nn
 
-class SimpleUNet(nn.Module):
-    """Simplified U-Net for demo purposes with batch normalization, LeakyReLU, dropout and sigmoid activation."""
-
-    def __init__(self, in_channels=3, out_channels=1, dropout_p=0.2):
-        super().__init__()
-
-        # Encoder (downsampling)
-        self.enc1 = self._conv_block(in_channels, 32, dropout_p)
-        self.enc2 = self._conv_block(32, 64, dropout_p)
-        self.enc3 = self._conv_block(64, 128, dropout_p)
-        self.enc4 = self._conv_block(128, 128, dropout_p)
-
-        # Decoder (upsampling)
-        self.dec4 = self._conv_block(128 + 128, 128, dropout_p)
-        self.dec3 = self._conv_block(128 + 64, 64, dropout_p)
-        self.dec2 = self._conv_block(64 + 32, 32, dropout_p)
-        self.dec1 = nn.Conv2d(32, out_channels, 1)
-
-        self.pool = nn.MaxPool2d(2)
-        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        self.sigmoid = nn.Sigmoid()  # Sigmoid activation for final output
-
-    def _conv_block(self, in_ch, out_ch, dropout_p=0.2):
-        """Conv block with batch normalization and LeakyReLU: Conv -> BN -> LeakyReLU -> Dropout -> Conv -> BN -> LeakyReLU -> Dropout"""
-        return nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, 3, padding=1),
-            nn.BatchNorm2d(out_ch),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            nn.Dropout2d(dropout_p),
-            nn.Conv2d(out_ch, out_ch, 3, padding=1),
-            nn.BatchNorm2d(out_ch),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            nn.Dropout2d(dropout_p)
-        )
-
-    def forward(self, x):
-        # Encoder
-        e1 = self.enc1(x)              # 256x256 -> 256x256
-        e2 = self.enc2(self.pool(e1))  # 256x256 -> 128x128
-        e3 = self.enc3(self.pool(e2))  # 128x128 -> 64x64
-        e4 = self.enc4(self.pool(e3))  # 64x64 -> 32x32
-
-        # Decoder with skip connections
-        d4 = self.dec4(torch.cat([self.upsample(e4), e3], 1)) # 32x32 -> 64x64
-        d3 = self.dec3(torch.cat([self.upsample(d4), e2], 1)) # 64x64 -> 128x128
-        d2 = self.dec2(torch.cat([self.upsample(d3), e1], 1)) # 128x128 -> 256x256
-        out = self.dec1(d2)
-
-        # Apply sigmoid activation to final output
-        out = self.sigmoid(out)
-
-        return out
-
-
 
 class ContextBlock(nn.Module):
     def __init__(self, channels, dropout_p):
